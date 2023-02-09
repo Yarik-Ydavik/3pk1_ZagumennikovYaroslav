da8b42b82e7a3ca24820f60619f362dc6eb4aaa2
Yaroslav Zagumennikov <yzagumennikov1@mail.ru>
2023-02-09 18:09:18 +0500

    пз2( Задание1 )

---
+++ PZ_2_(Zadanye_1)/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using PZ_2__Zadanye_1_;
+
+bool[,] edges = new bool[4, 4]
+{
+    {false, true, true, false},
+    {false, false, false, true},
+    {false, true, false, false},
+    {false, false, true, false},
+};
+
+Graph graph = new Graph(4, edges);
+
+graph.Depth(3);
